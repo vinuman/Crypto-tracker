ac11d87b809e2b258756ee6fb173a1bb7c5c5cc2
Vinu Cyril <vinucyril@gmail.com>
2023-11-08 16:06:50 +0530

    tabs component MUI

--- src/components/TabsComponent.js
+++ src/components/TabsComponent.js
@@ -1,11 +1,49 @@
-import React from "react";
+import React, { useState } from "react";
 
-const TabsComponent = () => {
+import Tab from "@mui/material/Tab";
+import TabContext from "@mui/lab/TabContext";
+import TabList from "@mui/lab/TabList";
+import TabPanel from "@mui/lab/TabPanel";
+import { createTheme, ThemeProvider } from "@mui/material";
+
+export default function TabsComponent() {
+  const [value, setValue] = useState("1");
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
+  const theme = createTheme({
+    palette: {
+      primary: {
+        main: "#3a80e9",
+      },
+    },
+  });
+
+  const style = {
+    color: "white",
+    fontSize: "20px",
+  };
+
   return (
-    <>
-      <div>TabsComponent</div>
-    </>
+    <ThemeProvider theme={theme}>
+      <div className=" bg-black">
+        <TabContext value={value}>
+          <div>
+            <TabList
+              variant="fullWidth"
+              onChange={handleChange}
+              aria-label="lab API tabs example"
+            >
+              <Tab label="Grid View" value="1" sx={style} />
+              <Tab label="List View" value="2" sx={style} />
+            </TabList>
+          </div>
+          <TabPanel value="1">Item One</TabPanel>
+          <TabPanel value="2">Item Two</TabPanel>
+        </TabContext>
+      </div>
+    </ThemeProvider>
   );
-};
-
-export default TabsComponent;
+}
