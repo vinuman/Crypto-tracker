db4084962db255df481ef7932adef3fecd93baca
Vinu Cyril <vinucyril@gmail.com>
2023-11-11 16:35:35 +0530

    coin info page

---
+++ src/components/CoinInfo.js
@@ -0,0 +1,31 @@
+import React, { useState } from "react";
+
+const CoinInfo = ({ title, desc }) => {
+  const [flag, setFlag] = useState(false);
+  const readableDesc =
+    desc.slice(0, 400) + "<span style='color:var(--grey)'> Read More...</span>";
+  const fullDesc =
+    desc + "<span style='color:var(--grey)'> Read Less...</span>";
+  return (
+    <>
+      <div className="p-8 bg-darkgrey m-[1.5rem]">
+        <h2 className="m-[1rem] text-[28px] font-bold">{title}</h2>
+
+        {desc.length > 300 ? (
+          <p
+            dangerouslySetInnerHTML={{ __html: flag ? fullDesc : readableDesc }}
+            className="m-[1rem] desc cursor-pointer transition-all duration-300"
+            onClick={() => setFlag(!flag)}
+          ></p>
+        ) : (
+          <p
+            dangerouslySetInnerHTML={{ __html: desc }}
+            className="m-[1rem] desc cursor-pointer transition-all duration-300"
+          ></p>
+        )}
+      </div>
+    </>
+  );
+};
+
+export default CoinInfo;
--- src/pages/CoinPage.js
+++ src/pages/CoinPage.js
@@ -6,6 +6,7 @@ import Loader from "../components/common/Loader";
 import Error from "../components/common/Error";
 import { coinObject } from "../functions/convertObject";
 import List from "../components/List";
+import CoinInfo from "../components/CoinInfo";
 
 const CoinPage = () => {
   const { id } = useParams();
@@ -43,6 +44,7 @@ const CoinPage = () => {
           <div className=" bg-darkgrey m-[1.5rem] rounded-lg">
             <List coin={coinData} />
           </div>
+          <CoinInfo title={coinData.name} desc={coinData.desc} />
         </>
       )}
     </>
